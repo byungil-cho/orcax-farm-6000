09b03d2df4f11de28c6e287fc3a6e401ff1fdb37
OrcaX <jbl01570@hanmail.net>
2025-04-19 19:01:04 +0900

    Update ecosystem.config.js

--- orcax-sms-backend/ecosystem.config.js
+++ orcax-sms-backend/ecosystem.config.js
@@ -2,14 +2,8 @@ module.exports = {
   apps: [
     {
       name: "orcax-sms-backend",
-      script: "./server.js", // ✅ 실제 실행할 진입점 파일!
+      script: "./server.js",
       watch: true,
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
     }
   ]
 }
