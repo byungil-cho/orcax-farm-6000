34496e994d47512f9c6a332ee9d3f58ceb672104
OrcaX <jbl01570@hanmail.net>
2025-04-21 00:59:42 +0900

    Add files via upload

---
+++ orcax-backend/src/index.mjs
@@ -0,0 +1,15 @@
+// index.js 또는 App.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient"; // 👈 위 파일 경로에 맞게 바꿔
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
