c2de0e36849041aade563d64641eb748973fa7be
OrcaX <jbl01570@hanmail.net>
2025-04-20 22:14:07 +0900

    Update index.js

--- orcax-backend/index.js
+++ orcax-backend/index.js
@@ -1,17 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-
-const app = express();
-
-app.use(cors()); // ❗️이거 없으면 끝장임
-
 import app from './server.js';
-import dotenv from 'dotenv';
 
-dotenv.config();
+const PORT = process.env.PORT || 3001;
 
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => {
-  console.log(`✅ Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
