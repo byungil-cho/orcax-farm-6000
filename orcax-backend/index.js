329b36506b8b80f9c3f947b71b12c2139079d9ac
OrcaX <jbl01570@hanmail.net>
2025-04-20 16:26:44 +0900

    Add files via upload

---
+++ orcax-backend/index.js
@@ -0,0 +1,11 @@
+// index.js
+import app from './server.js'; // server.js가 export 된 경우
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
