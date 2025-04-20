b0e4517f023a098808ee2d707eb37d5821734cab
OrcaX <jbl01570@hanmail.net>
2025-04-20 22:05:36 +0900

    Update index.js

--- orcax-backend/index.js
+++ orcax-backend/index.js
@@ -1,3 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors()); // ❗️이거 없으면 끝장임
+
 import app from './server.js';
 import dotenv from 'dotenv';
 
