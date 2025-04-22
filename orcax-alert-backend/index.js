36f960eb7f0395e847bfb362d8297b6c43567ed7
OrcaX <jbl01570@hanmail.net>
2025-04-22 20:50:50 +0900

    Update index.js

--- orcax-alert-backend/index.js
+++ orcax-alert-backend/index.js
@@ -1,4 +1,3 @@
-import cors from 'cors';
 import 'dotenv/config.js';
 import express from 'express';
 import { Connection, PublicKey } from '@solana/web3.js';
@@ -6,7 +5,6 @@ import nodemailer from 'nodemailer';
 import axios from 'axios';
 
 const app = express();
-app.use(cors()); 
 app.use(express.json());
 
 app.post('/order', (req, res) => {
