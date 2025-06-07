0907122ef1d51b094c6ea11eff8f804f9b7c6391
OrcaX <jbl01570@hanmail.net>
2025-06-08 03:11:48 +0900

    Update mongo.py

--- db/mongo.py
+++ db/mongo.py
@@ -1,10 +1,9 @@
-# db/mongo.py
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["orcax_db"]
 
+users = db["users"]
+
 def get_db():
     return db
-
-users = db["users"]
