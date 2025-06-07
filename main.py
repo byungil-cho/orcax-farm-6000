a445e10bd73f5fa41dd924a961c97a24903e570a
OrcaX <jbl01570@hanmail.net>
2025-06-08 03:10:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 # main.py
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-
-from api import plans
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
-)
 
-app.mount("/public", StaticFiles(directory="public"), name="public")
-app.include_router(plans.router)
+@app.get("/")
+def root():
+    return {"message": "OrcaX 6000 backend running."}
