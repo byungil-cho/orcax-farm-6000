7bd2357339280c00ef7a2f3e79f4bc82550ff066
OrcaX <jbl01570@hanmail.net>
2025-06-08 10:38:51 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,3 +9,6 @@ app.include_router(auth.router, prefix="/auth")
 @app.get("/")
 def root():
     return {"message": "OrcaX 감자 농장 서버 가동 중!"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=6000, reload=False)
