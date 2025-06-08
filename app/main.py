069e64bfbdb7cbbd853dac04513ec662ef74a6bd
OrcaX <jbl01570@hanmail.net>
2025-06-08 10:56:40 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from app.routes import auth
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+mongo_url = os.getenv("MONGO_URL")
+client = MongoClient(mongo_url)
+db = client["orcax"]
 
 app = FastAPI()
 
